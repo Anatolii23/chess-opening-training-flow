37c00f87bb3e8cdeb1cf7057ee8b974ae4e6a069
anatolii-hamza-axon <ahamza@axon.dev>
2025-12-27 11:15:54 +0100

    feat: clean up

--- backend/data/target/classes/flyway/V1__init_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE openings (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    player_color VARCHAR(50) NOT NULL,
-    move_tree_json TEXT
-);
-
-CREATE INDEX idx_openings_name ON openings(name);
